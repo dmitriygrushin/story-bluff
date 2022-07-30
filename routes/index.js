a18a5f1c60765c44e5a6134da20f1f4777fc8a7e
Dmitriy Grushin <dmitriy_grushin@yahoo.com>
2022-07-30 17:48:34 -0400

    Added: basic working routes with style - index and room

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/room', (req, res, next) => {
+    res.render('room');
 });
 
 module.exports = router;
