3bea76b92039df752929fe83058aec50ea1a4df7
Dmitriy Grushin <dmitriy_grushin@yahoo.com>
2022-08-01 19:38:57 -0400

    Added: onclick listener for all buttons and it changes the user's rating

--- public/javascripts/clientSocketIo.js
+++ public/javascripts/clientSocketIo.js
@@ -1,7 +1,26 @@
 const socket = io();
 
-socket.emit('join-room', roomId);
+const ratingButtons = document.getElementsByClassName('rate-button');
+const userListTag = document.getElementById('user-list');
 
-socket.on('message', function(msg) {
-    console.log(msg);
+socket.emit('join-room', roomId, username);
+
+socket.on('user-list', (userList) => {
+    // loop over userList hashmap and add each user to the userListTag
+    for (let userId in userList) {
+        let user = userList[userId];
+        let listItem = document.createElement('li');
+        listItem.style.fontSize = '1.3em';
+        if (user.rating == 0) user.rating = '✘';
+        listItem.innerHTML = `${user.username}: ${user.rating}`;
+        userListTag.appendChild(listItem);
+    }
 });
+
+
+// set onclick listener for all rating-buttons class
+for (let i = 0; i < ratingButtons.length; i++) {
+    ratingButtons[i].addEventListener('click', (e) => {
+        socket.emit('update-rating', e.target.value);
+    });
+}
--- socketApi.js
+++ socketApi.js
@@ -1,25 +1,46 @@
-var socket_io = require('socket.io');
-var io = socket_io();
-var socketApi = {};
+const socket_io = require('socket.io');
+const io = socket_io();
+let socketApi = {};
 
 socketApi.io = io;
 
 io.on('connection', (socket) => {
     console.log('A user connected');
-    socket.on('join-room', async (roomId) => {
-
-        console.log('a client is connected')
+    socket.on('join-room', async (roomId, username) => {
+        /** Initial Connection */
         socket.join(roomId); 
+        socket.data.user = {'username': username, 'rating': 0}; // initial user data. rating = 0 by default 
 
-        // on message from client send to all clients in the room
-        socket.on('message', function(msg){
-            console.log(msg);
-            io.to(roomId).emit('message', msg);
-        })
+        updateUserList(io, roomId);
+
+        socket.on('update-rating', (rating) => {
+            socket.data.user = {'username': username, 'rating': rating};
+            updateUserList(io, roomId);
+        });
+
+        socket.on('disconnect', () => {
+            io.to(roomId).emit('user-disconnected', socket.id);
+            updateUserList(io, roomId);
+        });
 
 
     });
-
 });
+
+/** Send updated user list to clients the room */
+async function updateUserList(io, roomId) {
+    let userList = await io.in(roomId).fetchSockets() 
+    let userListHashMap = {};
+    userList.forEach(socket => {
+        userListHashMap[socket.id] = socket.data.user;
+    });
+    io.to(roomId).emit('user-list', userListHashMap);
+}
+
+/*  60/40 have the server handle most of the workload and the client-side handle the rest.
+    There will be less back and forth between the server and the client so, its easier to implement.
+
+    Ideally, the client would handle most of the workload. Just send the data to the client 
+    and the client will take care of it.  */
 
 module.exports = socketApi;
